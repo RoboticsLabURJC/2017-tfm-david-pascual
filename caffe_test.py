edbb298633f12feb38d233a2d36a86d4cbae569d
dpascualhe <d.pascualhe@gmail.com>
2017-10-28 01:00:26 +0200

    Add Caffe config and prepare sample for Caffe model

--- caffe_test.py
+++ caffe_test.py
@@ -8,6 +8,34 @@ https://github.com/shihenw/convolutional-pose-machines-release/blob/master/testi
 __author__ = "David Pascual Hernandez"
 __date__ = "2017/10/2017"
 
+# Inserts paths to pycaffe and original CPMs repo modules
+import sys
+
+sys.path.insert(0, "/home/dpascualhe/caffe/python")
+sys.path.insert(0, "convolutional-pose-machines-release/testing/python/")
+
+import cv2 as cv
+import util
+from config_reader import config_reader
+
 if __name__ == '__main__':
-    #TODO adapt Caffe notebook
-    quit()
+    """
+    Getting the sample ready
+    """
+    sample = 'samples/nadal.png'
+    im = cv.imread(sample)
+    print("Loaded: " + sample)
+
+    # The image is resized to fit the input
+    param, model = config_reader()
+    boxsize = model['boxsize']  # image size used during training
+    scale = boxsize / float(im.shape[0])
+    test_im = cv.resize(im, (0, 0), fx=scale, fy=scale,
+                        interpolation=cv.INTER_CUBIC)
+
+    # Padded to become multiple of 8 (downsize factor of the CPM)
+    test_im, pad = util.padRightDownCorner(test_im)
+    print("Image resized: " + str(im.shape) + " -> " + str(test_im.shape))
+
+    # TODO load Caffe model
+    # TODO estimate pose
