adf25ab2edc0825199278d3d7feb8e41a7db62e8
dpascualhe <d.pascualhe@gmail.com>
2017-10-27 22:16:17 +0200

    Add scripts to test Caffe & TF implementations

---
+++ caffe_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+caffe_test.py: Script for testing CPMs original implementation with
+Caffe. Based on @shihenw code:
+https://github.com/shihenw/convolutional-pose-machines-release/blob/master/testing/python/demo.ipynb
+"""
+__author__ = "David Pascual Hernandez"
+__date__ = "2017/10/2017"
+
+if __name__ == '__main__':
+    #TODO adapt Caffe notebook
